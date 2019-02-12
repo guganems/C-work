5603d97ad6403270fc2221ccc0e614c6c65a5e2a
gnems <g_nemsveridze@cu.edu.ge>
2019-02-12 22:58:46 +0400

    Copying Memory in C

--- memory/memcpy_demo.c
+++ memory/memcpy_demo.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include<string.h>
+
+#define NODE_OFFSET 10
+#define BUFFER_SIZE 4096
 
 typedef struct node {
 	unsigned int isvalid : 1;
@@ -8,9 +12,13 @@ typedef struct node {
 
 int main(){
 	node_t n = {.isvalid = 1, .size = 367, .next = NULL};
-	node_t n2;
+	node_t *n2;
 	
-	n2.isvalid = n.isvalid;
+	char buffer[BUFFER_SIZE];
 	
-	printf("n2 = %d, %d, %p\n", n2.isvalid, n2.size, n2.next);
+	memset(buffer, 0xCD, BUFFER_SIZE);
+//	memcpy(buffer + NODE_OFFSET, &n, sizeof(node_t));
+	n2 = (node_t*)(buffer + NODE_OFFSET);
+	
+	printf("n2 = %d, %d, %p\n", n2->isvalid, n2->size, n2->next);
 }
